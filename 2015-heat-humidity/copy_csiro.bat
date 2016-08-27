a40aebd921a079d5b2579388cebeefacffcd841d
ecoffel <ecoffel@gmail.com>
2016-08-27 19:30:59 -0400

    bulk select

---
+++ 2015-heat-humidity/copy_csiro.bat
@@ -0,0 +1,19 @@
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r6i1p1\historical\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r6i1p1\historical\tasmax\regrid /MIR
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r6i1p1\rcp45\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r6i1p1\rcp45\tasmax\regrid /MIR
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r6i1p1\rcp85\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r6i1p1\rcp85\tasmax\regrid /MIR
+
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r7i1p1\historical\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r7i1p1\historical\tasmax\regrid /MIR
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r7i1p1\rcp45\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r7i1p1\rcp45\tasmax\regrid /MIR
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r7i1p1\rcp85\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r7i1p1\rcp85\tasmax\regrid /MIR
+
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r8i1p1\historical\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r8i1p1\historical\tasmax\regrid /MIR
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r8i1p1\rcp45\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r8i1p1\rcp45\tasmax\regrid /MIR
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r8i1p1\rcp85\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r8i1p1\rcp85\tasmax\regrid /MIR
+
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r9i1p1\historical\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r9i1p1\historical\tasmax\regrid /MIR
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r9i1p1\rcp45\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r9i1p1\rcp45\tasmax\regrid /MIR
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r9i1p1\rcp85\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r9i1p1\rcp85\tasmax\regrid /MIR
+
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r10i1p1\historical\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r10i1p1\historical\tasmax\regrid /MIR
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r10i1p1\rcp45\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r10i1p1\rcp45\tasmax\regrid /MIR
+robocopy f:\data\cmip5\output\csiro-mk3-6-0\r10i1p1\rcp85\tasmax\regrid e:\data\cmip5\output\csiro-mk3-6-0\r10i1p1\rcp85\tasmax\regrid /MIR
--- util/bulkSelectData.m
+++ util/bulkSelectData.m
@@ -1,22 +1,22 @@
-var = 'tasmax';
-models = {'csiro-mk3-6-0'};
+var = 'pr';
+models = {'bnu-esm', 'ccsm4', 'cnrm-cm5', 'hadgem2-es', 'noresm1-m'};
 %models = {''};
 rcps = {'historical', 'rcp85'};
 %rcps = {''};
 years = [1985:2080];
-ensembles = 1:10;
+ensembles = 1;
 
 inputBaseDir = 'e:\data\cmip5\output';
 %inputBaseDir = 'e:\data\ncep-reanalysis\output';
 outputBaseDir = 'c:\data';
 
-region = 'nyc';
+region = 'cambodia';
 
 for m = 1:length(models)
     for r = 1:length(rcps)
         for e = ensembles
 
-            inputDir = [inputBaseDir '\' models{m} '\r' num2str(e) 'i1p1\' rcps{r} '\' var '\regrid\world-bc\'];
+            inputDir = [inputBaseDir '\' models{m} '\r' num2str(e) 'i1p1\' rcps{r} '\' var '\regrid\' region '\'];
             %inputDir = [inputBaseDir '\' var '\regrid\'];
 
             dirNames = dir(inputDir);
